094d1fd748fd824119ff958ec474c348f02bca70
swikritisubedi <164749948+swikriti2222@users.noreply.github.com>
2026-07-18 08:30:20 +0545

    Add files via upload

---
+++ eventandselegate/delegate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace dotnetassignments
+{
+    class DelegateDemo
+    {
+        public delegate void Display();
+
+        public static void Message()
+        {
+            Console.WriteLine("Hello from Delegate");
+        }
+
+        public static void Run()
+        {
+            Console.WriteLine("----- DELEGATE -----");
+
+            Display d = Message;
+            d();
+
+            Console.WriteLine();
+        }
+    }
+
+    class @Delegate
+    {
+        static void Main(string[] args)
+        {
+            DelegateDemo.Run();
+        }
+    }
+}
---
+++ eventandselegate/event.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace dotnetassignments
+{
+    class Publisher
+    {
+        public delegate void Notify();
+
+        public event Notify ProcessCompleted;
+
+        public void StartProcess()
+        {
+            Console.WriteLine("Process Started...");
+
+            if (ProcessCompleted != null)
+                ProcessCompleted();
+        }
+    }
+
+    class Subscriber
+    {
+        public void Message()
+        {
+            Console.WriteLine("Event Received: Process Completed");
+        }
+    }
+
+    class EventDemo
+    {
+        public static void Run()
+        {
+            Console.WriteLine("----- EVENT -----");
+
+            Publisher p = new Publisher();
+            Subscriber s = new Subscriber();
+
+            p.ProcessCompleted += s.Message;
+
+            p.StartProcess();
+        }
+    }
+
+    class @Event
+    {
+        static void Main(string[] args)
+        {
+            EventDemo.Run();
+        }
+    }
+}
